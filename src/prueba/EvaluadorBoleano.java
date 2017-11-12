36c4d70ace3c71538d0acc35c7899e8150d8931e
jotaeze95 <jotaeze95@gmail.com>
2017-11-12 19:13:51 -0300

    Prueba 6 testParentesis Funciona!

--- src/prueba/EvaluadorBoleano.java
+++ src/prueba/EvaluadorBoleano.java
@@ -3,11 +3,10 @@ package prueba;
 public class EvaluadorBoleano {
 
 	public boolean eval(String expresion) {
-		if( "verdadero".equals(expresion)) {
-            return true;
-        } else {
-            return false;
+		if( expresion.startsWith("(") &&  expresion.endsWith(")")){
+            expresion = expresion.substring(1, expresion.length()-1);
         }
+        return "verdadero".equals(expresion);
     }
 
 }
