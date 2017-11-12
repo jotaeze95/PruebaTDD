94a0f6cdbcb01fcf2fc8ed922c24d452ffbd66f9
jotaeze95 <jotaeze95@gmail.com>
2017-11-12 18:20:37 -0300

    Clase Principal

---
+++ src/prueba/Principal.java
@@ -0,0 +1,14 @@
+package prueba;
+
+public class Principal {
+
+	public Principal() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
