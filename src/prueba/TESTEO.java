82dc31ae480140f63a9ecfc9e694f008aa8a5602
jotaeze95 <jotaeze95@gmail.com>
2017-11-12 18:13:24 -0300

    first commit

---
+++ src/prueba/TESTEO.java
@@ -0,0 +1,14 @@
+package prueba;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TESTEO {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
